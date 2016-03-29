28e35deeeb7622fa1828394617ecd1a38861e730
Kenji Okimoto <okimoto@clear-code.com>
2016-03-29 10:05:33 +0900

    Customize classes properly

--- init.rb
+++ init.rb
@@ -11,13 +11,17 @@ Rails.configuration.to_prepare do
   case
   when Redmine::Database.postgresql?
     require "full_text_search/pgroonga"
-    %w[projects news issues documents changesets messages journals wiki_pages custom_values].each do |name|
+    %w[projects news issues documents changesets messages wiki_pages].each do |name|
       name.classify.constantize.prepend(FullTextSearch::PGroonga)
     end
   when Redmine::Database.mysql?
     require "full_text_search/mroonga"
-    %w[projects news issues documents changesets messages journals wiki_pages custom_values].each do |name|
+    %w[projects news issues documents changesets messages wiki_pages].each do |name|
       name.classify.constantize.prepend(FullTextSearch::Mroonga)
+      name.classify.constantize.include(FullTextSearch::Mroonga)
+    end
+    %w[attachments custom_values journals wiki_contents].each do |name|
+      name.classify.constantize.include(FullTextSearch::Mroonga)
     end
   else
     # Do nothing
