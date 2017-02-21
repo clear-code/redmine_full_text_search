9e292ddcfbda71f0d625d4f8814113dcad412ac6
okkez <okkez000@gmail.com>
2017-02-21 23:06:03 +0900

    Set search result order properly when using top right search box
    Fix #5

--- lib/full_text_search/hooks/controller_search_index.rb
+++ lib/full_text_search/hooks/controller_search_index.rb
@@ -4,6 +4,8 @@ module FullTextSearch
       def index
         @full_text_search_order_target = params[:order_target].presence || "score"
         @full_text_search_order_type = params[:order_type].presence || "desc"
+        params[:order_target] = @full_text_search_order_target
+        params[:order_type] = @full_text_search_order_type
         super
       end
     end
