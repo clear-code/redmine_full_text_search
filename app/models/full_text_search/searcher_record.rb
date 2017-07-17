976b5a051409f9f13b8b466f6b0b6e51467f572e
okkez <okkez000@gmail.com>
2017-07-17 17:38:16 +0900

    Add some methods to FullTextSearch::SearcherRecord

--- app/models/full_text_search/searcher_record.rb
+++ app/models/full_text_search/searcher_record.rb
@@ -1,4 +1,33 @@
 module FullTextSearch
   class SearcherRecord < ActiveRecord::Base
+    class << self
+      def from_record(record_hash)
+        h = record_hash.dup
+        h.delete("_id")
+        h.delete("ctid")
+        new(h)
+      end
+
+      def target_columns
+        %i[
+          name
+          identifier
+          description
+          title
+          summary
+          subject
+          comments
+          content
+          notes
+          text
+          value
+          filename
+        ]
+      end
+    end
+
+    def similar_search(limit: 10)
+      # TODO
+    end
   end
 end
