73eea27a207970e7621adad9b8127629a6e409a3
Kenji Okimoto <okimoto@clear-code.com>
2017-07-19 12:58:36 +0900

    Copy SearchController#index to prepare parameters

--- lib/full_text_search/hooks/controller_search_index.rb
+++ lib/full_text_search/hooks/controller_search_index.rb
@@ -6,7 +6,55 @@ module FullTextSearch
         @full_text_search_order_type = params[:order_type].presence || "desc"
         params[:order_target] = @full_text_search_order_target
         params[:order_type] = @full_text_search_order_type
-        super
+
+        # Copy from SearchController
+        @question = params[:q] || ""
+        @question.strip!
+        @all_words = params[:all_words] ? params[:all_words].present? : true
+        @titles_only = params[:titles_only] ? params[:titles_only].present? : false
+        @search_attachments = params[:attachments].presence || '0'
+        @open_issues = params[:open_issues] ? params[:open_issues].present? : false
+
+        case params[:format]
+        when 'xml', 'json'
+          @offset, @limit = api_offset_and_limit
+        else
+          @offset = nil
+          @limit = Setting.search_results_per_page.to_i
+          @limit = 10 if @limit == 0
+        end
+
+        # quick jump to an issue
+        if (m = @question.match(/^#?(\d+)$/)) && (issue = Issue.visible.find_by_id(m[1].to_i))
+          redirect_to issue_path(issue)
+          return
+        end
+
+        projects_to_search =
+          case params[:scope]
+          when 'all'
+            nil
+          when 'my_projects'
+            User.current.projects
+          when 'subprojects'
+            @project ? (@project.self_and_descendants.active.to_a) : nil
+          else
+            @project
+          end
+
+        @object_types = Redmine::Search.available_search_types.dup
+        if projects_to_search.is_a? Project
+          # don't search projects
+          @object_types.delete('projects')
+          # only show what the user is allowed to view
+          @object_types = @object_types.select {|o| User.current.allowed_to?("view_#{o}".to_sym, projects_to_search)}
+        end
+
+        @scope = @object_types.select {|t| params[t]}
+        @scope = @object_types if @scope.empty?
+
+        searcher = FullTextSearch::Searcher.new
+        @search_result = searcher.search
       end
     end
   end
