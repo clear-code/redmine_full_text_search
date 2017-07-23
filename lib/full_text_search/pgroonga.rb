cb5030094e34c7d5a7313f9b0ab49ae5ccba3b2c
okkez <okkez000@gmail.com>
2017-07-23 16:34:57 +0900

    Filter by project ids

--- lib/full_text_search/pgroonga.rb
+++ lib/full_text_search/pgroonga.rb
@@ -5,10 +5,32 @@ module FullTextSearch
     end
 
     module ClassMethods
-      def search(query, offset: nil, limit: 10, all_words: true, order_target: "score", order_type: "desc")
+      #
+      #
+      # @params order_target "score" or "date"
+      # @params order_type   "desc" or "asc"
+      def search(query,
+                 project_ids: [],
+                 offset: nil,
+                 limit: 10,
+                 all_words: true,
+                 order_target: "score",
+                 order_type: "desc")
         unless all_words
           query = query.split(" ").join(" OR ")
         end
+        project_condition = if project_ids.empty?
+                              ""
+                            else
+                              " && in_values(project_id, #{project_ids}.join(', '))"
+                            end
+        sort_direction = order_type == "desc" ? "-" : ""
+        sort_keys = case order_target
+                    when "score"
+                      "#{sort_direction}_score"
+                    when "date"
+                      "#{sort_direction}original_updated_on, #{sort_direction}original_created_on"
+                    end
         sql = <<-SQL
           select pgroonga.command(
                    'select',
@@ -17,7 +39,9 @@ module FullTextSearch
                      'output_columns', '*,_score',
                      'drilldown', 'original_type',
                      'match_columns', '#{target_columns.join(",")}',
-                     'query', pgroonga.query_escape('#{query}')
+                     'query', pgroonga.query_escape('#{query}'),
+                     'filter', 'pgroonga_tuple_is_alive(ctid)#{project_condition}',
+                     'sort_keys', '#{sort_keys}'
                    ]
                  )::json
         SQL
