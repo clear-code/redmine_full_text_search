a496be028b8baf4165c136144bc269eed6640915
okkez <okkez000@gmail.com>
2017-07-30 18:21:42 +0900

    Organize method signature

--- lib/full_text_search/mroonga.rb
+++ lib/full_text_search/mroonga.rb
@@ -5,19 +5,30 @@ module FullTextSearch
     end
 
     module ClassMethods
-      def search(query, offset: nil, limit: 10, all_words: true, order_target: "score", order_type: "desc")
+      def search(query,
+                 user: User.current,
+                 project_ids: [],
+                 scope: [],
+                 attachments: "0",
+                 all_words: true,
+                 titles_only: false,
+                 offset: nil,
+                 limit: 10,
+                 order_target: "score",
+                 order_type: "desc")
         unless all_words
           query = query.split(" ").join(" OR ")
         end
         # mroonga_command cannot contain new line
-        # FIXME: escape query
+        # mroonga_command can accept multiple arguments since 7.0.5
         sql = [
           "select mroonga_command(",
           "'select --table searcher_records",
           "--output_columns *,_score",
           "--drilldown original_type",
-          "--match_columns #{target_columns.join(',')}",
+          "--match_columns #{target_columns.join('||')}",
           "--query \"#{query}\"",
+          "--filter #{filter_condition(user, project_ids, scope, attachments)}",
           "'",
           ")"
         ].join(" ")
