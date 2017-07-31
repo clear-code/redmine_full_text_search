28c39ca0040fea70fd653d64754f438c345e65e3
Kenji Okimoto <okimoto@clear-code.com>
2017-07-31 16:48:05 +0900

    Remove fts_* tables

--- db/migrate/20170630062526_reset_schema_for_mroonga.rb
+++ db/migrate/20170630062526_reset_schema_for_mroonga.rb
@@ -1,39 +1,16 @@
 class ResetSchemaForMroonga < ActiveRecord::Migration
   def change
     return unless Redmine::Database.mysql?
-    reversible do |d|
-      d.up do
-        remove_index(:fts_projects, :name)
-        remove_index(:fts_projects, :identifier)
-        remove_index(:fts_projects, :description)
-        remove_index(:fts_news, :title)
-        remove_index(:fts_news, :summary)
-        remove_index(:fts_news, :description)
-        remove_index(:fts_issues, :subject)
-        remove_index(:fts_issues, :description)
-        remove_index(:fts_documents, :title)
-        remove_index(:fts_documents, :description)
-        remove_index(:fts_messages, :subject)
-        remove_index(:fts_messages, :content)
-        remove_index(:fts_attachments, :filename)
-        remove_index(:fts_attachments, :description)
-      end
-      d.down do
-        add_index(:fts_projects, :name, type: "fulltext")
-        add_index(:fts_projects, :identifier, type: "fulltext")
-        add_index(:fts_projects, :description, type: "fulltext")
-        add_index(:fts_news, :title, type: "fulltext")
-        add_index(:fts_news, :summary, type: "fulltext")
-        add_index(:fts_news, :description, type: "fulltext")
-        add_index(:fts_issues, :subject, type: "fulltext")
-        add_index(:fts_issues, :description, type: "fulltext")
-        add_index(:fts_documents, :title, type: "fulltext")
-        add_index(:fts_documents, :description, type: "fulltext")
-        add_index(:fts_messages, :subject, type: "fulltext")
-        add_index(:fts_messages, :content, type: "fulltext")
-        add_index(:fts_attachments, :filename, type: "fulltext")
-        add_index(:fts_attachments, :description, type: "fulltext")
-      end
-    end
+    drop_table(:fts_projects, if_exists: true)
+    drop_table(:fts_news, if_exists: true)
+    drop_table(:fts_issues, if_exists: true)
+    drop_table(:fts_journals, if_exists: true)
+    drop_table(:fts_documents, if_exists: true)
+    drop_table(:fts_messages, if_exists: true)
+    drop_table(:fts_attachments, if_exists: true)
+    drop_table(:fts_changesets, if_exists: true)
+    drop_table(:fts_wiki_pages, if_exists: true)
+    drop_table(:fts_wiki_contents, if_exists: true)
+    drop_table(:fts_custom_values, if_exists: true)
   end
 end
