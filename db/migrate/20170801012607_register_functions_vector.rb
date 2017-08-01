fcd9988c36f350c7835f6c1a85af66215f0e4094
Kenji Okimoto <okimoto@clear-code.com>
2017-08-01 10:50:23 +0900

    Add migration to register plugin functions/vector

---
+++ db/migrate/20170801012607_register_functions_vector.rb
@@ -0,0 +1,28 @@
+class RegisterFunctionsVector < ActiveRecord::Migration
+  def change
+    reversibele do |d|
+      d.up do
+        command = "plugin_register functions/vector"
+        case
+        when Redmine::Database.postgresql?
+          execute("select pgroonga.command('#{command}')")
+        when Redmine::Database.mysql?
+          execute("select mroonga_command('#{command}')")
+        else
+          # do nothing
+        end
+      end
+      d.down do
+        command = "plugin_unregister functions/vector"
+        case
+        when Redmine::Database.postgresql?
+          execute("select pgroonga.command('#{command}')")
+        when Redmine::Database.mysql?
+          execute("select mroonga_command('#{command}')")
+        else
+          # do nothing
+        end
+      end
+    end
+  end
+end
