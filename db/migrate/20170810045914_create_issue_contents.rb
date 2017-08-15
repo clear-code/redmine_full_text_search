a5bfb76713930d3a0e7313cbdc8146ec245fc0b9
Kenji Okimoto <okimoto@clear-code.com>
2017-08-15 17:28:41 +0900

    Add missing columns to display proper similar issues

--- db/migrate/20170810045914_create_issue_contents.rb
+++ db/migrate/20170810045914_create_issue_contents.rb
@@ -5,15 +5,21 @@ class CreateIssueContents < ActiveRecord::Migration
         case
         when Redmine::Database.postgresql?
           create_table :issue_contents do |t|
+            t.integer :project_id
             t.integer :issue_id, unique: true, null: false
             t.string :subject
             t.text :contents
+            t.integer :status_id
+            t.boolean :is_private
           end
         when Redmine::Database.mysql?
           create_table :issue_contents, options: "ENGINE=Mroonga" do |t|
+            t.integer :project_id
             t.integer :issue_id, unique: true, null: false
             t.string :subject
             t.text :contents, limit: 16.megabytes
+            t.integer :status_id
+            t.boolean :is_private
           end
         end
         load_data
@@ -32,7 +38,14 @@ class CreateIssueContents < ActiveRecord::Migration
     (0..n_pages).each do |offset|
       Issue.eager_load(:journals).limit(1000).offset(offset * 1000).each do |issue|
         contents = [issue.subject, issue.description] + issue.journals.sort_by(&:id).map(&:notes)
-        FullTextSearch::IssueContent.create(issue_id: issue.id, subject: issue.subject, contents: contents.join("\n"))
+        FullTextSearch::IssueContent.create(
+          project_id: issue.project_id,
+          issue_id: issue.id,
+          subject: issue.subject,
+          contents: contents.join("\n"),
+          status_id: issue.status_id,
+          is_private: issue.is_private
+        )
       end
     end
   end
