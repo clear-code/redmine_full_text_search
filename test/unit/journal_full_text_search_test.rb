4a1f51643ccf2891ad4e00482822df6afc97953d
Kouhei Sutou <kou@clear-code.com>
2019-02-08 16:18:19 +0900

    Add a test for save/destroy journal

--- test/unit/journal_full_text_search_test.rb
+++ test/unit/journal_full_text_search_test.rb
@@ -1,47 +1,48 @@
 require File.expand_path('../../test_helper', __FILE__)
 
-class IssueFullTextSearchTest < ActiveSupport::TestCase
+class JournalFullTextSearchTest < ActiveSupport::TestCase
   make_my_diffs_pretty!
 
   fixtures :enumerations
   fixtures :issue_statuses
+  fixtures :issues
   fixtures :projects
   fixtures :projects_trackers
   fixtures :trackers
   fixtures :users
 
   def test_save
-    issue = Issue.generate!
-    issue.reload
+    journal = Journal.generate!
+    journal.reload
     records = FullTextSearch::SearcherRecord.
-                where(original_id: issue.id,
-                      original_type: issue.class.name)
+                where(original_id: journal.id,
+                      original_type: journal.class.name)
     assert_equal([
                    {
-                     "project_id" => issue.project_id,
-                     "project_name" => issue.project.name,
-                     "original_id" => issue.id,
-                     "original_type" => issue.class.name,
-                     "original_created_on" => issue.created_on,
-                     "original_updated_on" => issue.updated_on,
+                     "project_id" => journal.issue.project_id,
+                     "project_name" => journal.issue.project.name,
+                     "original_id" => journal.id,
+                     "original_type" => journal.class.name,
+                     "original_created_on" => journal.created_on,
+                     "original_updated_on" => nil,
                      "name" => nil,
-                     "description" => issue.description,
+                     "description" => nil,
                      "identifier" => nil,
                      "status" => nil,
                      "title" => nil,
                      "summary" => nil,
-                     "tracker_id" => issue.tracker_id,
-                     "subject" => issue.subject,
-                     "author_id" => issue.author_id,
-                     "is_private" => issue.is_private,
-                     "status_id" => issue.status_id,
-                     "issue_id" => issue.id,
+                     "tracker_id" => nil,
+                     "subject" => nil,
+                     "author_id" => journal.user_id,
+                     "is_private" => nil,
+                     "status_id" => journal.journalized.status_id,
+                     "issue_id" => journal.journalized_id,
                      "comments" => nil,
                      "short_comments" => nil,
                      "long_comments" => nil,
                      "content" => nil,
                      "notes" => nil,
-                     "private_notes" => nil,
+                     "private_notes" => journal.private_notes,
                      "text" => nil,
                      "value" => nil,
                      "custom_field_id" => nil,
@@ -54,12 +55,12 @@ class IssueFullTextSearchTest < ActiveSupport::TestCase
   end
 
   def test_destroy
-    issue = Issue.generate!
+    journal = Journal.generate!
     records = FullTextSearch::SearcherRecord.
-                where(original_id: issue.id,
-                      original_type: issue.class.name)
+                where(original_id: journal.id,
+                      original_type: journal.class.name)
     assert_equal(1, records.size)
-    issue.destroy!
+    journal.destroy!
     assert_equal([], records.reload.to_a)
   end
 end
