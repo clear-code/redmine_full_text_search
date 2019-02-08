ca1644c18975a8d26ab23da9fbf8a456ffe7fab0
Kouhei Sutou <kou@clear-code.com>
2019-02-08 19:08:20 +0900

    Extract target classes

--- init.rb
+++ init.rb
@@ -1,8 +1,3 @@
-require_dependency "full_text_search/hooks/search_index_options_content_bottom_hook"
-require_dependency "full_text_search/hooks/issues_show_description_bottom_hook"
-require_dependency "full_text_search/hooks/similar_issues_helper"
-require "full_text_search/searcher"
-
 Redmine::Plugin.register :full_text_search do
   name 'Full Text Search plugin'
   author 'Kenji Okimoto'
@@ -15,17 +10,23 @@ Redmine::Plugin.register :full_text_search do
 end
 
 Rails.configuration.to_prepare do
+  require_dependency "full_text_search"
+  require_dependency "full_text_search/hooks/search_index_options_content_bottom_hook"
+  require_dependency "full_text_search/hooks/issues_show_description_bottom_hook"
+  require_dependency "full_text_search/hooks/similar_issues_helper"
+  require_dependency "full_text_search/searcher"
+
   case
   when Redmine::Database.postgresql?
-    require "full_text_search/pgroonga"
+    require_dependency "full_text_search/pgroonga"
     FullTextSearch::SearcherRecord.prepend(FullTextSearch::PGroonga)
   when Redmine::Database.mysql?
-    require "full_text_search/mroonga"
+    require_dependency "full_text_search/mroonga"
     FullTextSearch::SearcherRecord.prepend(FullTextSearch::Mroonga)
   else
     # Do nothing
   end
-  [Project, News, Issue, Document, Changeset, Message, Journal, WikiPage, WikiContent, CustomValue, Attachment].each do |klass|
+  FullTextSearch.target_classes.each do |klass|
     klass.include(FullTextSearch::Model)
   end
   Issue.include(FullTextSearch::SimilarSearcher::Model)
--- lib/full_text_search.rb
+++ lib/full_text_search.rb
@@ -1,2 +1,19 @@
 module FullTextSearch
+  class << self
+    def target_classes
+      [
+        Attachment,
+        Changeset,
+        CustomValue,
+        Document,
+        Issue,
+        Journal,
+        Message,
+        News,
+        Project,
+        WikiContent,
+        WikiPage,
+      ]
+    end
+  end
 end
