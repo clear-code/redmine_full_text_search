328c45dcad01f8b070ff87b85609e9a54bf0e93a
Kouhei Sutou <kou@clear-code.com>
2019-02-12 18:49:05 +0900

    test: add support for Redmine 4.0 + Mroonga

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -39,22 +39,26 @@ module NullValues
 
   def null_datetime
     if mroonga?
-      connection = ActiveRecord::Base.connection
-      db_time_zone =
-        connection.execute("SHOW VARIABLES LIKE 'time_zone'").first[1]
-      if db_time_zone == "SYSTEM"
+      if Rails::VERSION::MAJOR >= 5
+        nil
+      else
+        connection = ActiveRecord::Base.connection
         db_time_zone =
-          connection.execute("SHOW VARIABLES LIKE 'system_time_zone'").first[1]
-      end
-      utc_offset = 0
-      TZInfo::Timezone.all.each do |zone|
-        period = zone.current_period
-        if period.abbreviation == db_time_zone.to_sym
-          utc_offset = period.offset.utc_offset
-          break
+          connection.execute("SHOW VARIABLES LIKE 'time_zone'").first[1]
+        if db_time_zone == "SYSTEM"
+          db_time_zone =
+            connection.execute("SHOW VARIABLES LIKE 'system_time_zone'").first[1]
         end
+        utc_offset = 0
+        TZInfo::Timezone.all.each do |zone|
+          period = zone.current_period
+          if period.abbreviation == db_time_zone.to_sym
+            utc_offset = period.offset.utc_offset
+            break
+          end
+        end
+        Time.at(0 - utc_offset).in_time_zone
       end
-      Time.at(0 - utc_offset).in_time_zone
     else
       nil
     end
