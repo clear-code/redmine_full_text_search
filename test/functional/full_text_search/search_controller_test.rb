67ef9490e430d22098734af26410c395133cf0a3
Kouhei Sutou <kou@clear-code.com>
2019-02-12 15:38:22 +0900

    Add a test for issue search

--- test/functional/full_text_search/search_controller_test.rb
+++ test/functional/full_text_search/search_controller_test.rb
@@ -8,9 +8,10 @@ module FullTextSearch
 
     tests SearchController
 
+    fixtures :enabled_modules
     fixtures :enumerations
-    fixtures :issues
     fixtures :issue_statuses
+    fixtures :issues
     fixtures :projects
     fixtures :projects_trackers
     fixtures :trackers
@@ -19,12 +20,40 @@ module FullTextSearch
     def setup
       SearcherRecord.sync
       execute_groonga_command("plugin_register functions/vector")
+      @request.session[:user_id] = User.admin.first.id
     end
 
-    def test_search
-      get :index
-      assert_select("#options-content") do
-        assert_select(".full-text-search-order")
+    def format_issue_titles(issues)
+      issues.collect do |issue|
+        "#{issue.tracker.name} \##{issue.id} (#{issue.status.name}): " +
+          "#{issue.subject}"
+      end
+    end
+
+    class OptionsTest < self
+      def test_index
+        get :index
+        assert_select("#options-content") do
+          assert_select(".full-text-search-order")
+        end
+      end
+    end
+
+    class IssueTest < self
+      def search(query)
+        get :index, params: {"q" => query, "issues" => "1"}
+      end
+
+      def test_search
+        search("print OR private")
+        issues = [
+          Issue.find(6),
+          Issue.find(1),
+        ]
+        assert_select("#search-results") do
+          assert_equal(format_issue_titles(issues),
+                       css_select("dt a").collect(&:text))
+        end
       end
     end
   end
