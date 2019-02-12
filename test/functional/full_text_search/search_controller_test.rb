49322523e78e5987c9e77f483b242a582c7ec958
Kouhei Sutou <kou@clear-code.com>
2019-02-12 16:38:04 +0900

    Add a test for custom field search

--- test/functional/full_text_search/search_controller_test.rb
+++ test/functional/full_text_search/search_controller_test.rb
@@ -8,6 +8,10 @@ module FullTextSearch
 
     tests SearchController
 
+    fixtures :custom_fields
+    fixtures :custom_fields_projects
+    fixtures :custom_fields_trackers
+    fixtures :custom_values
     fixtures :enabled_modules
     fixtures :enumerations
     fixtures :issue_statuses
@@ -52,6 +56,35 @@ module FullTextSearch
         ]
         assert_select("#search-results") do
           assert_equal(format_issue_titles(issues),
+                       css_select("dt a").collect(&:text))
+        end
+      end
+    end
+
+    class CustomFieldTest < self
+      def search(query, params={})
+        get :index, params: {"q" => query, "issues" => "1"}.merge(params)
+      end
+
+      def generate_issue!(project, custom_field_values)
+        issue = Issue.generate!(project: project)
+        issue.custom_field_values = custom_field_values
+        issue.save!
+        issue
+      end
+
+      def test_scoped
+        searchable_custom_field = CustomField.where(searchable: true).first
+        tracker = searchable_custom_field.trackers.first
+        project1, project2, = tracker.projects
+        custom_field_values = {
+          searchable_custom_field.id => "Searchable",
+        }
+        issue1 = generate_issue!(project1, custom_field_values)
+        issue2 = generate_issue!(project2, custom_field_values)
+        search("searchable", id: project1.id)
+        assert_select("#search-results") do
+          assert_equal(format_issue_titles([issue1]),
                        css_select("dt a").collect(&:text))
         end
       end
