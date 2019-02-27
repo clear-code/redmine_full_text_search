b9fc8e3429e728214b1aefd6f587ae2391aa433e
Kouhei Sutou <kou@clear-code.com>
2019-02-27 18:34:26 +0900

    Restrict max memory usage of external command as system memory / 4

--- config/initializers/chupa_text.rb
+++ config/initializers/chupa_text.rb
@@ -2,3 +2,15 @@ ChupaText.logger = Rails.logger
 ChupaText::Decomposers.enable_all_gems
 ChupaText::Decomposers.load
 ChupaText::Configuration.default
+if File.readable?("/proc/meminfo")
+  File.open("/proc/meminfo") do |meminfo|
+    meminfo.each_line do |line|
+      case line
+      when /\AMemTotal:\s+(\d+) kB/
+        total_memory = Integer($1, 10)
+        ENV["CHUPA_TEXT_EXTERNAL_COMMAND_LIMIT_AS"] ||= "#{total_memory / 4}KB"
+        break
+      end
+    end
+  end
+end
