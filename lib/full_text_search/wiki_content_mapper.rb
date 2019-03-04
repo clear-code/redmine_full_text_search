83b3cbf8f96463c786f494e51581739cafa07a39
Kouhei Sutou <kou@clear-code.com>
2019-03-04 09:49:36 +0900

    Add missing options

--- lib/full_text_search/wiki_content_mapper.rb
+++ lib/full_text_search/wiki_content_mapper.rb
@@ -13,7 +13,7 @@ module FullTextSearch
   resolver.register(WikiContent, WikiContentMapper)
 
   class RedmineWikiContentMapper < RedmineMapper
-    def upsert_searcher_record
+    def upsert_searcher_record(options={})
       searcher_record = find_searcher_record
       searcher_record.original_id = @record.page_id
       searcher_record.original_type = "WikiPage"
--- lib/full_text_search/wiki_page_mapper.rb
+++ lib/full_text_search/wiki_page_mapper.rb
@@ -13,7 +13,7 @@ module FullTextSearch
   resolver.register(WikiPage, WikiPageMapper)
 
   class RedmineWikiPageMapper < RedmineMapper
-    def upsert_searcher_record
+    def upsert_searcher_record(options={})
       searcher_record = find_searcher_record
       searcher_record.original_id = @record.id
       searcher_record.original_type = @record.class.name
