7b3712cca4e3869806b8728ac57229bc2da8967f
Kouhei Sutou <kou@clear-code.com>
2019-03-29 17:51:24 +0900

    Use methods provided by Redmine

--- lib/full_text_search/changeset_mapper.rb
+++ lib/full_text_search/changeset_mapper.rb
@@ -14,15 +14,14 @@ module FullTextSearch
 
   class RedmineChangesetMapper < RedmineMapper
     def upsert_searcher_record(options={})
-      short_comments, long_comments = @record.comments.split(/(?:\r?\n)+/, 2).map(&:strip)
       searcher_record = find_searcher_record
       searcher_record.original_id = @record.id
       searcher_record.original_type = @record.class.name
       searcher_record.project_id = @record.repository.project_id
       searcher_record.project_name = @record.repository.project.name
       searcher_record.comments = @record.comments
-      searcher_record.short_comments = short_comments
-      searcher_record.long_comments = long_comments
+      searcher_record.short_comments = @record.short_comments&.strip
+      searcher_record.long_comments = @record.long_comments&.strip
       searcher_record.original_created_on = @record.committed_on
       searcher_record.save!
     end
