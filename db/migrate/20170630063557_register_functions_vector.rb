9330f018f4776271a52f4194831b44c1e0f76012
Sutou Kouhei <kou@clear-code.com>
2019-05-28 16:59:06 +0900

    Ensure enabling PGroonga

--- db/migrate/20170630063557_register_functions_vector.rb
+++ db/migrate/20170630063557_register_functions_vector.rb
@@ -2,5 +2,12 @@ migration = ActiveRecord::Migration
 migration = migration[4.2] if migration.respond_to?(:[])
 class RegisterFunctionsVector < migration
   def change
+    reversible do |d|
+      d.up do
+        if Redmine::Database.postgresql?
+          enable_extension("pgroonga") unless extension_enabled?("pgroonga")
+        end
+      end
+    end
   end
 end
