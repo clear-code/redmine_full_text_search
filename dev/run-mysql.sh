14fc167f17a288587b22e64ef0d2844e967f5351
Sutou Kouhei <kou@clear-code.com>
2019-05-29 16:21:48 +0900

    Add a script to run MySQL

---
+++ dev/run-mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -exu
+
+options=(--rm -p3306:3306)
+if [ $# -ge 1 ]; then
+  db_dir=$1
+  options+=("-v${db_dir}:/var/lib/mysql")
+fi
+
+db_conf_dir=/tmp/redmine-full-text-search/my.cnf.d
+mkdir -p ${db_conf_dir}
+cat <<MY_CNF > ${db_conf_dir}/local.cnf
+[mysqld]
+max_allowed_packet = 256M
+MY_CNF
+options+=("-v${db_conf_dir}:/etc/my.cnf.d")
+
+docker run "${options[@]}" groonga/mroonga:latest
