77bd6401a8780534573e0127b672312b3660489f
Sutou Kouhei <kou@clear-code.com>
2019-07-14 15:21:06 +0900

    Simplify

--- lib/full_text_search/mroonga.rb
+++ lib/full_text_search/mroonga.rb
@@ -60,25 +60,15 @@ SHOW VARIABLES LIKE 'mroonga_libgroonga_version';
         mysql_system_time_zone = connection.select_rows(<<-SQL)[0][1]
 SHOW VARIABLES LIKE 'system_time_zone'
         SQL
-        now = Time.now
-        utc_offset = now.utc_offset
-        now.utc
-        begin
-          timezone = TZInfo::Timezone.get(mysql_system_time_zone)
-        rescue TZInfo::InvalidTimezoneIdentifier
-          timezone = TZInfo::Timezone.all.find do |tz|
-            abbreviation = tz.period_for_utc(now).abbreviation
-            abbreviation.to_s == mysql_system_time_zone
+        mysql_system_utc_offset = 0
+        TZInfo::Timezone.all.each do |timezone|
+          period = timezone.current_period
+          if period.abbreviation.to_s == mysql_system_time_zone
+            mysql_system_utc_offset = period.utc_offset
+            break
           end
         end
-        if timezone.nil?
-          Rails.logger.warn("[full-text-search][mroonga] " +
-                            "unknown timeonze: <#{mysql_system_time_zone}>")
-          mysql_system_utc_offset = 0
-        else
-          mysql_system_utc_offset = timezone.period_for_utc(now).utc_offset
-        end
-        mysql_system_utc_offset - utc_offset
+        mysql_system_utc_offset - Time.now.utc_offset
       end
     end
   end
