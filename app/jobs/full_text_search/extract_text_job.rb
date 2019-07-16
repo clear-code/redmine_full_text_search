9f3759edd7f8be21af90208904a273c3ae1c42b8
Sutou Kouhei <kou@clear-code.com>
2019-07-16 13:18:25 +0900

    Discard "record not found" jobs immediately

--- app/jobs/full_text_search/extract_text_job.rb
+++ app/jobs/full_text_search/extract_text_job.rb
@@ -3,6 +3,8 @@ module FullTextSearch
     queue_as :full_text_search
     queue_with_priority 15
 
+    discard_on ActiveRecord::RecordNotFound
+
     def perform(id)
       target = Target.find(id)
       mapper = target.mapper.redmine_mapper
--- app/jobs/full_text_search/upsert_target_job.rb
+++ app/jobs/full_text_search/upsert_target_job.rb
@@ -3,6 +3,8 @@ module FullTextSearch
     queue_as :full_text_search
     queue_with_priority 15
 
+    discard_on ActiveRecord::RecordNotFound
+
     def perform(mapper_class_name, source_id)
       mapper_class = mapper_class_name.constantize
       source = mapper_class.redmine_class.find(source_id)
