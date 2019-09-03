ebff5c19f4274603b79e51e852c9fa8dba2fa193
Sutou Kouhei <kou@clear-code.com>
2019-09-03 18:46:06 +0900

    log-analyzer: add support for analyzing search

--- lib/full_text_search/log_analyzer.rb
+++ lib/full_text_search/log_analyzer.rb
@@ -1,12 +1,23 @@
 require "English"
 require "optparse"
 require "ostruct"
+require "json"
+require "time"
 
 module FullTextSearch
   class LogAnalyzer
     class Options < Struct.new(:large_memory_usage_diff_threshold,
                                :large_memory_usage_threshold,
-                               :slow_elapsed_time_threshold)
+                               :slow_elapsed_time_threshold,
+                               :report_text_extraction_statistics,
+                               :report_search_statistics)
+      def report_text_extraction_statistics?
+        report_text_extraction_statistics
+      end
+
+      def report_search_statistics?
+        report_search_statistics
+      end
     end
 
     class Size
@@ -162,7 +173,7 @@ module FullTextSearch
       end
     end
 
-    class Statistics
+    class TextExtractionStatistics
       attr_reader :n_records
       attr_reader :n_slow_records
       attr_reader :total
@@ -193,12 +204,93 @@ module FullTextSearch
       end
     end
 
+    class SearchStatistics
+      attr_reader :history
+      attr_reader :zero_hits
+      attr_reader :slow_searches
+      attr_reader :n_searches
+      attr_reader :n_visits
+      attr_reader :max
+      attr_reader :min
+      attr_reader :mean
+      def initialize
+        @history = []
+        @zero_hits = []
+        @slow_searches = []
+        @n_searches = 0
+        @n_visits = 0
+        @max = nil
+        @min = nil
+        @mean = ElapsedTime.new(0.0)
+      end
+
+      def searched(action)
+        @n_searches += 1
+        elapsed_time = ElapsedTime.new(action["elapsed_time"])
+        if @max.nil? or elapsed_time > @max
+          @max = elapsed_time
+        end
+        if @min.nil? or elapsed_time < @min
+          @min = elapsed_time
+        end
+        @mean += ((elapsed_time - @mean) / @n_searches)
+        @history << action
+        if action["n_hits"].zero?
+          @zero_hits << action
+        end
+        if elapsed_time > 1.0
+          @slow_searches << action
+        end
+      end
+
+      def visited(parameters)
+        @n_visits += 1
+        @history << parameters
+      end
+
+      def conversions
+        each_conversions.to_a
+      end
+
+      def each_conversions
+        return enum_for(__method__) unless block_given?
+        next_action_threshold = 30
+        good_threshold = 3
+        last_timestamp = nil
+        last_search = nil
+        last_visit = nil
+        @history.each do |data|
+          timestamp = data["timestamp"]
+          choose_n = data["search_n"]
+          if choose_n
+            last_visit = data
+          else
+            if last_timestamp and
+              last_visit and
+              timestamp + next_action_threshold > last_timestamp and
+              last_visit["search_n"] < good_threshold
+              yield(last_search, last_visit)
+            end
+            last_search = data
+            last_visit = nil
+          end
+          last_timestamp = timestamp
+        end
+        if last_visit and last_visit["search_n"] < good_threshold
+          yield(last_search, last_visit)
+        end
+      end
+    end
+
     def initialize
       @options = Options.new
       @options.large_memory_usage_diff_threshold = Size.new("512MiB")
       @options.large_memory_usage_threshold = Size.new("1GiB")
       @options.slow_elapsed_time_threshold = ElapsedTime.new("5s")
+      @options.report_text_extraction_statistics = true
+      @options.report_search_statistics = true
       @extension_statistics = {}
+      @search_statistics = {}
     end
 
     def run(argv)
@@ -227,6 +319,14 @@ module FullTextSearch
     private
     def create_option_parser
       option_parser = OptionParser.new
+      option_parser.on("--no-report-text-extraction-statistics",
+                       "Don't report text extraction statistics") do |boolean|
+        @options.report_text_extraction_statistics = boolean
+      end
+      option_parser.on("--no-report-search-statistics",
+                       "Don't report search statistics") do |boolean|
+        @options.report_search_statistics = boolean
+      end
       option_parser.on("--large-memory-usage-diff-threshold=THRESHOLD",
                        Integer,
                        "Use THRESHOLD as the threshold " +
@@ -258,7 +358,35 @@ module FullTextSearch
       Record.new(label, attributes, @options)
     end
 
+    def parse_action(raw_action)
+      action = JSON.parse(raw_action)
+      action["timestamp"] = Time.iso8601(action["timestamp"])
+      action
+    end
+
+    def parse_parameters(raw_parameters)
+      # TODO: security risk
+      parameters = BasicObject.new.instance_eval(raw_parameters)
+      if parameters["search_n"]
+        parameters["search_n"] = Integer(parameters["search_n"], 10)
+      end
+      parameters
+    end
+
+    def parse_user(raw_user)
+      case raw_user
+      when /([a-zA-Z0-9_-]+) \(id=(\d+)\)\z/
+        name = $1
+        id = $2
+        [name, Integer(id, 10)]
+      else
+        nil
+      end
+    end
+
     def analyze(input)
+      last_parameters = nil
+      last_request_timestamp = nil
       input.each_line do |line|
         line = line.chomp
         original_line = line
@@ -269,7 +397,9 @@ module FullTextSearch
           content = $POSTMATCH
           case content
           when /\A\[text-extract\] /
-            record = parse_record($POSTMATCH)
+            raw_record = $POSTMATCH
+            next unless @options.report_text_extraction_statistics?
+            record = parse_record(raw_record)
             case record.label
             when "Extracted"
               extracted(record)
@@ -296,7 +426,9 @@ module FullTextSearch
               end
             end
           when /\A\[repository-entry\] /
-            record = parse_record($POSTMATCH)
+            raw_record = $POSTMATCH
+            next unless @options.report_text_extraction_statistics?
+            record = parse_record(raw_record)
             if record.slow?
               puts("slow: %s:%s:%s:%s" % [
                      record.elapsed_time,
@@ -306,12 +438,43 @@ module FullTextSearch
                    ])
             end
           when "Failed to extract text"
+            next unless @options.report_text_extraction_statistics?
             case components[11]
             when "ChupaText::EncryptedError"
             else
               puts("error: #{line}")
             end
+          when /\A\[search\] /
+            raw_action = $POSTMATCH
+            next unless @options.report_search_statistics?
+            action = parse_action(raw_action)
+            searched(action)
           end
+        when /\AStarted GET ".+?" for [\d.]+ at /
+          raw_timestamp = $POSTMATCH
+          next unless @options.report_search_statistics?
+          last_request_timestamp = Time.parse(raw_timestamp)
+        when /\A  Parameters: /
+          raw_parameters = $POSTMATCH
+          next unless @options.report_search_statistics?
+          last_parameters = nil
+          parameters = parse_parameters(raw_parameters)
+          next unless parameters["search_id"]
+          next unless parameters["search_n"]
+          last_parameters = parameters
+        when /\A  Current user: /
+          raw_user = $POSTMATCH
+          next unless @options.report_search_statistics?
+          next unless last_request_timestamp
+          next unless last_parameters
+          user_name, user_id = parse_user(raw_user)
+          next if user_id.nil?
+          last_parameters["user_name"] = user_name
+          last_parameters["user_id"] = user_id
+          last_parameters["timestamp"] = last_request_timestamp
+          visited(last_parameters)
+          last_timestamp = nil
+          last_parameters = nil
         end
       end
     end
@@ -320,12 +483,32 @@ module FullTextSearch
       extension = File.extname(record.path || "").gsub(/\A\./, "")
       return if extension.empty?
       extension = extension.downcase
-      @extension_statistics[extension] ||= Statistics.new
+      @extension_statistics[extension] ||= TextExtractionStatistics.new
       @extension_statistics[extension].add(record)
     end
 
+    def searched(action)
+      @search_statistics[action["user_id"]] ||= SearchStatistics.new
+      @search_statistics[action["user_id"]].searched(action)
+    end
+
+    def visited(parameters)
+      statistics = @search_statistics[parameters["user_id"]]
+      return if statistics.nil?
+      statistics.visited(parameters)
+    end
+
     def report
-      puts("Summary:")
+      if @options.report_text_extraction_statistics?
+        report_text_extraction_statistics
+      end
+      if @options.report_search_statistics?
+        report_search_statistics
+      end
+    end
+
+    def report_text_extraction_statistics
+      puts("Text extraction summary:")
       sorted = @extension_statistics.sort_by do |extension, statistics|
         -statistics.total.to_f
       end
@@ -337,6 +520,53 @@ module FullTextSearch
         puts("    Max:            #{statistics.max}")
         puts("    Min:            #{statistics.min}")
         puts("    Mean:           #{statistics.mean}")
+      end
+    end
+
+    def report_search_statistics
+      puts("Search summary:")
+      puts("  N users: #{@search_statistics.size}")
+      sorted = @search_statistics.sort_by do |user_id, statistics|
+        -statistics.history.size
+      end
+      sorted.each_with_index do |(user_id, statistics), i|
+        user_name = nil
+        statistics.history.each do |data|
+          user_name = data["user_name"]
+          break if user_name
+        end
+        puts("  #{i}: #{user_name} (#{user_id})")
+        puts("    N searches:       #{statistics.n_searches}")
+        puts("    N visits:         #{statistics.n_visits}")
+        puts("    Max search time:  #{statistics.max}")
+        puts("    Min search time:  #{statistics.min}")
+        puts("    Mean search time: #{statistics.mean}")
+
+        conversions = statistics.conversions
+        puts("    N conversions:    #{conversions.size}")
+        conversions.each_with_index do |(action, visit), j|
+          query = action["q"]
+          n = visit["search_n"]
+          puts("      #{j}: #{query}: #{n}")
+        end
+
+        zero_hits = statistics.zero_hits
+        puts("    N zero hits:      #{zero_hits.size}")
+        zero_hits.each_with_index do |action, j|
+          query = action["q"]
+          puts("      #{j}: #{query}")
+        end
+
+        slow_searches = statistics.slow_searches.sort_by do |action|
+          -action["elapsed_time"]
+        end
+        puts("    N slow searches:  #{slow_searches.size}")
+        slow_searches.each_with_index do |action, j|
+          elapsed_time = ElapsedTime.new(action["elapsed_time"])
+          puts("      #{j}: #{elapsed_time}")
+          pp action
+        end
+        pp statistics.history
       end
     end
   end
