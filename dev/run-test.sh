4000fa862cd2031af2416707969206d838404d05
Sutou Kouhei <kou@clear-code.com>
2022-04-12 10:01:37 +0900

    dev: improve test runner with Redmine 5.0

--- dev/run-test.sh
+++ dev/run-test.sh
@@ -2,7 +2,15 @@
 
 set -exu
 
-env \
-  PSQLRC=/tmp/nonexistent \
-  RAILS_ENV=test \
-    ${RUBY:-ruby} bin/rails redmine:plugins:test "$@"
+rails_version_major=$(grep "^gem 'rails'" Gemfile | grep -o '[0-9]*' | head -n1)
+if [ ${rails_version_major} -ge 6 ]; then
+  env \
+    PSQLRC=/tmp/nonexistent \
+    RAILS_ENV=test \
+      ${RUBY:-ruby} bin/rails test "plugins/*/test/**/*_test.rb" "$@"
+else
+  env \
+    PSQLRC=/tmp/nonexistent \
+    RAILS_ENV=test \
+      ${RUBY:-ruby} bin/rails redmine:plugins:test "$@"
+fi
