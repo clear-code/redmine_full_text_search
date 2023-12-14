7f617ec645dd25287d87d02ff90585f31b900967
otegami <a.s.takuya1026@gmail.com>
2023-12-14 21:09:02 +0800

    Simplified the SQL queries

--- db/migrate/20231210061330_add_created_at_to_fts_targets_with_index.rb
+++ db/migrate/20231210061330_add_created_at_to_fts_targets_with_index.rb
@@ -7,16 +7,11 @@ class AddCreatedAtToFtsTargetsWithIndex < ActiveRecord::Migration[5.2]
 
     ActiveRecord::Base.transaction do
       update_created_at_for_changes
-      update_created_at_for_custom_values
-      update_created_at("Attachment", "attachments", "created_on")
-      update_created_at("Changeset", "changesets", "committed_on")
-      update_created_at("Document", "documents", "created_on")
-      update_created_at("Issue", "issues", "created_on")
-      update_created_at("Journal", "journals", "created_on")
-      update_created_at("Message", "messages", "created_on")
-      update_created_at("News", "news", "created_on")
-      update_created_at("Project", "projects", "created_on")
-      update_created_at("WikiPage", "wiki_pages", "created_on")
+      update_created_at_using_last_modified_at
+      update_created_at_using_created_on("Issue", "issues")
+      update_created_at_using_created_on("Message", "messages")
+      update_created_at_using_created_on("Project", "projects")
+      update_created_at_using_created_on("WikiPage", "wiki_pages")
     end
 
     if Redmine::Database.mysql?
@@ -79,26 +74,18 @@ class AddCreatedAtToFtsTargetsWithIndex < ActiveRecord::Migration[5.2]
     SQL
   end
 
-  def update_created_at_for_custom_values
+  def update_created_at_using_last_modified_at
     execute <<-SQL
       UPDATE fts_targets
-      SET created_at = (SELECT created_on FROM issues WHERE issues.id = (SELECT customized_id FROM custom_values WHERE custom_values.id = fts_targets.source_id AND customized_type = 'Issue'))
-      WHERE fts_targets.source_type_id = (SELECT id FROM fts_types WHERE name = 'CustomValue') AND
-            EXISTS (SELECT 1 FROM custom_values WHERE custom_values.id = fts_targets.source_id AND customized_type = 'Issue')
-    SQL
-
-    execute <<-SQL
-      UPDATE fts_targets
-      SET created_at = (SELECT created_on FROM projects WHERE projects.id = (SELECT customized_id FROM custom_values WHERE custom_values.id = fts_targets.source_id AND customized_type = 'Project'))
-      WHERE fts_targets.source_type_id = (SELECT id FROM fts_types WHERE name = 'CustomValue') AND
-            EXISTS (SELECT 1 FROM custom_values WHERE custom_values.id = fts_targets.source_id AND customized_type = 'Project')
+      SET created_at = last_modified_at
+      WHERE fts_targets.source_type_id IN (SELECT id FROM fts_types WHERE name IN ('CustomValue', 'Attachment', 'Changeset', 'Document', 'Journal', 'News'));
     SQL
   end
 
-  def update_created_at(type_name, table_name, timestamp_column)
+  def update_created_at_using_created_on(type_name, table_name)
     execute <<-SQL
       UPDATE fts_targets
-      SET created_at = (SELECT #{timestamp_column} FROM #{table_name} WHERE #{table_name}.id = fts_targets.source_id)
+      SET created_at = (SELECT created_on FROM #{table_name} WHERE #{table_name}.id = fts_targets.source_id)
       WHERE fts_targets.source_type_id = (SELECT id FROM fts_types WHERE name = '#{type_name}')
     SQL
   end
