7c313815a49158b88ecaed06eb070a280213ca60
otegami <a.s.takuya1026@gmail.com>
2024-02-27 22:21:35 +0900

    In Rails 7, the .schema_format= API is moved from ActiveRecord::Base to ActiveRecord
    ref: https://github.com/rails/rails/pull/42447

--- config/initializers/schema_format.rb
+++ config/initializers/schema_format.rb
@@ -1,4 +1,5 @@
 # This is just for running test.
 # This isn't used normal use case.
 # Redmine doesn't use plugins/*/config/initializers/*.rb
-ActiveRecord::Base.schema_format = :sql
+active_record = ActiveRecord.respond_to?(:schema_format=) ? ActiveRecord : ActiveRecord::Base
+active_record.schema_format = :sql
