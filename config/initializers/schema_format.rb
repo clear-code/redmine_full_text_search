0794bdb72062f7489b1c1715eba68520f13db90e
takuya kodama <a.s.takuya1026@gmail.com>
2024-03-05 07:38:45 +0800

    Adjust `.schema_format=` API Usage for Rails 7 Compatibility (#128)
    related: #126 
    
    In Rails 7, the .schema_format= API is moved from ActiveRecord::Base to
    ActiveRecord
    - ref: https://github.com/rails/rails/pull/42445
    
    It also solved the following CI failure.
    ```    
    NoMethodError: undefined method `schema_format=' for ActiveRecord::Base:Class
    ```

--- config/initializers/schema_format.rb
+++ config/initializers/schema_format.rb
@@ -1,4 +1,9 @@
 # This is just for running test.
 # This isn't used normal use case.
 # Redmine doesn't use plugins/*/config/initializers/*.rb
-ActiveRecord::Base.schema_format = :sql
+if ActiveRecord.respond_to?(:schema_format=)
+  # For Rails >= 7
+  ActiveRecord.schema_format = :sql
+else
+  ActiveRecord::Base.schema_format = :sql
+end
