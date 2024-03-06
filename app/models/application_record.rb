bbf3003284e31299ce9dd0cc89b168d83b8328e2
Sutou Kouhei <kou@cozmixng.org>
2024-03-07 08:42:23 +0900

    Add missing =

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 # For backward compatibility with Redmine < 6.
-# ApplicationRecord is inherited from ActiveRecord Models instead of ActiveRecord::Base in Redmine > 6.
+# ApplicationRecord is inherited from ActiveRecord Models instead of ActiveRecord::Base in Redmine >= 6.
 # ref: https://www.redmine.org/issues/38975
 unless defined?(ApplicationRecord)
   ApplicationRecord = ActiveRecord::Base
