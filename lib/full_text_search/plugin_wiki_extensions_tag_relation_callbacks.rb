75cbad966b80733638029446e8b82026b05d6caa
Abe Tomoaki <abe@clear-code.com>
2024-04-01 17:43:13 +0900

    Refactoring
    * `target` -> `fts_target`
    * Attach with class_eval

--- lib/full_text_search/plugin_wiki_extensions_tag_relation_callbacks.rb
+++ lib/full_text_search/plugin_wiki_extensions_tag_relation_callbacks.rb
@@ -5,29 +5,31 @@ module FullTextSearch
 
     class << self
       def attach
-        WikiExtensionsTagRelation.after_create_commit(self)
-        WikiExtensionsTagRelation.after_destroy(self)
+        mapper_class = self
+        WikiExtensionsTagRelation.class_eval do
+          after_commit mapper_class, on: [:create]
+          after_destroy mapper_class
+        end
       end
 
       def after_commit(record)
-        target = Target.find_by(
+        fts_target = Target.find_by(
           source_id: record.wiki_page_id,
           source_type_id: Type.wiki_page.id)
         tag_id = Tag.label(record.tag.name).id
-        unless target.tag_ids.include?(tag_id)
-          target.tag_ids = target.tag_ids.concat([tag_id])
-          target.save!
+        unless fts_target.tag_ids.include?(tag_id)
+          fts_target.tag_ids = fts_target.tag_ids.concat([tag_id])
+          fts_target.save!
         end
       end
-      alias_method :after_create_commit, :after_commit
 
       def after_destroy(record)
-        target = Target.find_by(
+        fts_target = Target.find_by(
           source_id: record.wiki_page_id,
           source_type_id: Type.wiki_page.id)
-        tag_ids = target.tag_ids - [Tag.label(record.tag.name).id]
-        target.tag_ids = tag_ids
-        target.save!
+        tag_ids = fts_target.tag_ids - [Tag.label(record.tag.name).id]
+        fts_target.tag_ids = tag_ids
+        fts_target.save!
       end
     end
   end
