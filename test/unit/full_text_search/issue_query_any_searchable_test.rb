4eb49343c1efe6c649279f363f7d8f9895678343
otegami <otegami@clear-code.com>
2025-03-04 17:16:18 +0900

    add test cases using bookmarked or my project filters

--- test/unit/full_text_search/issue_query_any_searchable_test.rb
+++ test/unit/full_text_search/issue_query_any_searchable_test.rb
@@ -6,6 +6,7 @@ module FullTextSearch
       unless IssueQuery.method_defined?(:sql_for_any_searchable_field)
         skip("Required feature 'sql_for_any_searchable_field' does not exist.")
       end
+      User.current = nil
     end
 
     def test_or_one_word
@@ -61,6 +62,97 @@ module FullTextSearch
       expected_issues = [
         subject_groonga_description_pgroonga,
         subject_pgroonga_description_groonga,
+        subject_pgroonga_journal_groonga.issue
+      ]
+      assert_equal(expected_issues, query.issues)
+    end
+
+    def test_and_two_words_within_my_projects
+      Issue.destroy_all
+      my_user = User.find(1)
+      project = Project.generate!
+      User.add_to_project(my_user, project)
+
+      # User's project issues.
+      subject_groonga_description_pgroonga =
+        Issue.generate!(project: project,
+                        subject: "ぐるんが",
+                        description: "ぴーじーるんが")
+      without_keywords = Issue.generate!(project: project,
+                                         subject: "no-keyword",
+                                         description: "no-keyword")
+      subject_pgroonga_journal_groonga =
+        Issue.generate!(project: project, subject: "ぴーじーるんが")
+             .journals.create!(notes: "ぐるんが")
+      # Another project issue.
+      subject_pgroonga_description_groonga =
+             Issue.generate!(subject: "ぴーじーるんが",
+                             description: "ぐるんが")
+
+      User.current = my_user
+      query = IssueQuery.new(
+        :name => "_",
+        :filters => {
+          "any_searchable" => {
+            :operator => "~",
+            :values => ["ぐるんが ぴーじーるんが"]
+          },
+          "project_id" => {
+            :operator => "=",
+            :values => ['mine']
+          },
+        },
+        :sort_criteria => [["id", "asc"]]
+      )
+      expected_issues = [
+        subject_groonga_description_pgroonga,
+        subject_pgroonga_journal_groonga.issue
+      ]
+      assert_equal(expected_issues, query.issues)
+    end
+
+    def test_and_two_words_within_bookmarks
+      Issue.destroy_all
+      bookmark_user = User.find(1)
+      bookmarked_project =
+        Project.where(id: [bookmark_user.bookmarked_project_ids])
+               .first
+      no_bookmarked_project = Project.generate!
+
+      # User's bookmarked project issues.
+      subject_groonga_description_pgroonga =
+        Issue.generate!(project: bookmarked_project,
+                        subject: "ぐるんが",
+                        description: "ぴーじーるんが")
+      without_keywords = Issue.generate!(project: bookmarked_project,
+                                         subject: "no-keyword",
+                                         description: "no-keyword")
+      subject_pgroonga_journal_groonga =
+        Issue.generate!(project: bookmarked_project, subject: "ぴーじーるんが")
+             .journals.create!(notes: "ぐるんが")
+      # Another project issue.
+      subject_pgroonga_description_groonga =
+             Issue.generate!(project: no_bookmarked_project,
+                             subject: "ぴーじーるんが",
+                             description: "ぐるんが")
+
+      User.current = bookmark_user
+      query = IssueQuery.new(
+        :name => "_",
+        :filters => {
+          "any_searchable" => {
+            :operator => "~",
+            :values => ["ぐるんが ぴーじーるんが"]
+          },
+          "project_id" => {
+            :operator => "=",
+            :values => ["bookmarks"]
+          },
+        },
+        :sort_criteria => [["id", "asc"]]
+      )
+      expected_issues = [
+        subject_groonga_description_pgroonga,
         subject_pgroonga_journal_groonga.issue
       ]
       assert_equal(expected_issues, query.issues)
