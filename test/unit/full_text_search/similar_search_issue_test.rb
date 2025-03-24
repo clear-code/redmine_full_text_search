4493e2c5c0c6af8c51db815a033525fa3d0c520f
otegami <otegami@clear-code.com>
2025-03-24 15:04:12 +0900

    performs enqueued FullTextSearch::UpdateIssueContentJob jobs

--- test/unit/full_text_search/similar_search_issue_test.rb
+++ test/unit/full_text_search/similar_search_issue_test.rb
@@ -2,6 +2,8 @@ require File.expand_path("../../../test_helper", __FILE__)
 
 module FullTextSearch
   class SimilarSearchIssueTest < ActiveSupport::TestCase
+    include ActiveJob::TestHelper
+
     def setup
       User.current = User.find(1)
       @project = Project.generate!
@@ -10,15 +12,19 @@ module FullTextSearch
 
     def test_same_structure_on_issue
       fts_engine_groonga_open_source =
-        Issue.generate!(
-          project: @project,
-          subject: "ぐるんが",
-          description: "高速に検索できます。 オープンソースです。")
+        perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+          Issue.generate!(
+            project: @project,
+            subject: "ぐるんが",
+            description: "高速に検索できます。 オープンソースです。")
+        end
       fts_engine_pgroonga_open_source =
-        Issue.generate!(
-          project: @project,
-          subject: "ぴーじーるんが",
-          description: "PostgreSQLに組み込んで高速に検索できます。 オープンソースです。")
+        perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+          Issue.generate!(
+            project: @project,
+            subject: "ぴーじーるんが",
+            description: "PostgreSQLに組み込んで高速に検索できます。 オープンソースです。")
+        end
 
       similar_issues =
         fts_engine_groonga_open_source.similar_issues(
@@ -26,7 +32,9 @@ module FullTextSearch
       assert_equal([fts_engine_pgroonga_open_source],
                    similar_issues)
 
-      fts_engine_groonga_open_source.update!(description: nil)
+      perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+        fts_engine_groonga_open_source.update!(description: nil)
+      end
       similar_issues =
         fts_engine_groonga_open_source.similar_issues(
           project_ids: [@project.id])
@@ -35,19 +43,24 @@ module FullTextSearch
 
     def test_same_structure_with_journal
       fts_engine_groonga_open_source =
-        Issue.generate!(project: @project, subject: "ぐるんが")
-             .journals.create!(notes: "高速に検索できます。 オープンソースです。")
+        perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+          Issue.generate!(project: @project, subject: "ぐるんが")
+               .journals.create!(notes: "高速に検索できます。 オープンソースです。")
+        end
       fts_engine_pgroonga_open_source =
-        Issue.generate!(project: @project, subject: "ぴーじーるんが")
-             .journals.create!(
-                notes: "PostgreSQLに組み込んで高速に検索できます。 オープンソースです。")
-
+        perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+          Issue.generate!(project: @project, subject: "ぴーじーるんが")
+               .journals.create!(
+                 notes: "PostgreSQLに組み込んで高速に検索できます。 オープンソースです。")
+        end
       target_issue = fts_engine_groonga_open_source.issue
       similar_issues = target_issue.similar_issues(project_ids: [@project.id])
       assert_equal([fts_engine_pgroonga_open_source.issue],
                    similar_issues)
 
-      target_issue.journals.first.destroy
+      perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+        target_issue.journals.first.destroy
+      end
       similar_issues =
         target_issue.reload.similar_issues(project_ids: [@project.id])
       assert_equal([], similar_issues)
@@ -64,7 +77,9 @@ module FullTextSearch
               :original_filename => "Groonga 解説資料 最新版")}
         }
       )
-      fts_engine_groonga_latest_file.save!
+      perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+        fts_engine_groonga_latest_file.save!
+      end
       fts_engine_pgroonga_latest_file =
         Issue.generate!(project: @project, subject: "ぴーじーるんが")
       fts_engine_pgroonga_latest_file.save_attachments(
@@ -74,15 +89,18 @@ module FullTextSearch
               :original_filename => "PGroonga 解説資料 最新版")}
         }
       )
-      fts_engine_pgroonga_latest_file.save!
-
+      perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+        fts_engine_pgroonga_latest_file.save!
+      end
       similar_issues =
         fts_engine_groonga_latest_file
           .similar_issues(project_ids: [@project.id])
       assert_equal([fts_engine_pgroonga_latest_file],
                    similar_issues)
 
-      fts_engine_groonga_latest_file.attachments.first.destroy
+      perform_enqueued_jobs(only: FullTextSearch::UpdateIssueContentJob) do
+        fts_engine_groonga_latest_file.attachments.first.destroy
+      end
       similar_issues =
         fts_engine_groonga_latest_file
           .reload
